b5087c057eb8a8fb7f4e08046c5788ab200ff246
Missael Armenta <missael306@gmail.com>
2022-05-19 13:56:21 -0500

    Maqueta inicial librerias y estructura general

---
+++ Cotizador/Models/GlobalResources.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cotizador.Models
+{
+    public class GlobalResources
+    {
+        public static string GetResource(string File, string Key, string Default)
+        {
+            try
+            {
+                return HttpContext.GetGlobalResourceObject(File, Key).ToString();
+            }
+            catch (Exception)
+            {
+
+                return Default;
+            }
+        }
+    }
+}
