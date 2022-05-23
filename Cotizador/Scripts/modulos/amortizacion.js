9430056dddb7535bcb379434b12a6f5c102f71cf
Missael Armenta <missael306@gmail.com>
2022-05-23 11:50:31 -0500

    Datatable integrado

---
+++ Cotizador/Scripts/modulos/amortizacion.js
@@ -0,0 +1,34 @@
+﻿var Cotizador = Cotizador || {};
+Cotizador.Amortizacion = Cotizador.Amortizacion || {};
+
+Cotizador.Amortizacion = (function () {
+    "use strict";
+    let AmortizacionLoad = function () {
+        let Comun = Cotizador.Comun;
+
+        this.initialize = function () {
+            iniTable();
+        };
+
+        let iniTable = function () {
+            $("#amortizationTable").DataTable({
+                "searching": true,
+                "ordering": false,
+                "info": false,
+                "dom": "t<'bottom'ip>",
+                "language": {
+                    "url": "/Scripts/plugins/datatable_language.json"
+                },
+            });
+        }
+  
+    };
+
+    return new AmortizacionLoad();
+})();
+(function ($, window, document) {
+    "use strict";
+    $(function () {
+        Cotizador.Amortizacion.initialize();
+    });
+})(window.jQuery, window, document);
